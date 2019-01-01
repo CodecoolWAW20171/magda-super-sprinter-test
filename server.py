33b4e4c0b64e33bd9383628751d8a46be59ff12d
magg666 <mw23127@gmail.com>
2019-01-01 16:43:42 +0100

    added another page function

--- server.py
+++ server.py
@@ -9,8 +9,14 @@ app = Flask(__name__)
 @app.route('/list')
 def route_list():
     user_stories = data_handler.get_all_user_story()
+    headers = data_handler.DATA_HEADER
+    return render_template('list.html', user_stories=user_stories, headers=headers)
 
-    return render_template('list.html', user_stories=user_stories)
+
+@app.route('/add')
+def route_add():
+    return render_template('add.html')
+
 
 if __name__ == '__main__':
     app.run(
