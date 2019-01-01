9b1566162a25901fbecc546772ffef4ea4537303
magg666 <mw23127@gmail.com>
2019-01-01 16:43:20 +0100

    added functions to append stories and read stories

--- data_handler.py
+++ data_handler.py
@@ -7,4 +7,20 @@ STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
 
 def get_all_user_story():
-    return []
+    with open('data.csv', newline='') as file:
+        reader = csv.DictReader(file)
+        all_stories = []
+        for row in reader:
+            all_stories.append(row)
+        return all_stories
+
+
+def append_stories_in_file(story):
+    with open('data.csv', 'a', newline='') as file:
+        fieldnames = DATA_HEADER
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(story)
+
+
+# story1 = {'id':'01', 'title':'all list', 'user_story':'As a User', 'acceptance_criteria':'Given that', 'business_value':'20', 'estimation':'2', 'status':'done'}
+# append_stories_in_file(story1)
