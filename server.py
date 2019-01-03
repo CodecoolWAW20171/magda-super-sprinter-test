cf71bbb596d600641eadcdfb4d20e018005d4b43
magg666 <mw23127@gmail.com>
2019-01-03 11:45:09 +0100

    added saving from form function

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 import data_handler
 
@@ -13,9 +13,24 @@ def route_list():
     return render_template('list.html', user_stories=user_stories, headers=headers)
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def route_add():
-    return render_template('add.html')
+    statuses = data_handler.STATUSES
+    story = None
+    if request.method == 'GET':
+        return render_template('add.html', statuses=statuses)
+    else:  # czy to jest prawidłowo czy nie powinno byc ze wskazaniem na post?
+        id = request.form['id']
+        title = request.form['title']
+        user_story = request.form['user_story']
+        acceptance_criteria = request.form['acceptance_criteria']
+        business_value = request.form['business_value']
+        estimation = request.form['estimation']
+        status = request.form['status']
+        story = {'id': id, 'title': title, 'user_story': user_story, 'acceptance_criteria': acceptance_criteria,
+                 'business_value': business_value, 'estimation': estimation, 'status': status}
+        data_handler.append_stories_in_file(story)
+        return redirect('/')
 
 
 if __name__ == '__main__':
