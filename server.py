027dccf5d7613ffaa492c5e75494632bbd6b4758
magg666 <mw23127@gmail.com>
2019-01-04 14:54:00 +0100

    added comments and docstrings. all working. supports /, /list, /story, /story<id> - main, adding, updating

--- server.py
+++ server.py
@@ -1,48 +1,65 @@
+# file to handling server and passing data between user and database
 from flask import Flask, render_template, request, redirect, url_for, session
-
 import data_handler
+import common
 
 app = Flask(__name__)
 
 
+# można wyrzucić stąd główną stronę i zrobić stronę wyboru projektu albo
+# logowanie jako główną. Do przemyślenia
 @app.route('/')
 @app.route('/list')
 def route_list():
+    """
+    Function supports main page and /list page
+    :return: main page with list of stories
+    """
+
     user_stories = data_handler.get_all_user_story()
     headers = ['ID', 'TITLE', 'USER STORY', 'ACCEPTANCE CRITERIA', 'BUSINESS VALUE', 'ESTIMATION', 'STATUS']
     return render_template('list.html', user_stories=user_stories, headers=headers)
 
 
+# W tej funkcji użyłam funkcji z common:
+# generowania id jako znacznika czasowego i tworzenia słownika z danymi ze strony
+# wątpliwość - jako pierwszą metodę wskazałam GET natomiat POST bez konkretnego wskazania
+# czy to dobrze?
 @app.route('/story', methods=['GET', 'POST'])
 def route_add():
+    """
+    Function supports /add page - adding data
+    Using two methods - if GET - shows forms to fill
+    if POST - execute saving data in database and redirect to /list page
+    :return: depends of used method (but webpage)
+    """
     statuses = data_handler.STATUSES
-    story = None
     if request.method == 'GET':
         return render_template('add-story.html', statuses=statuses)
-    else:  # czy to jest prawidłowo czy nie powinno byc ze wskazaniem na post?
-        id = request.form['id']
-        title = request.form['title']
-        user_story = request.form['user_story']
-        acceptance_criteria = request.form['acceptance_criteria']
-        business_value = request.form['business_value'] + ' point'
-        estimation = request.form['estimation'] + ' h'
-        status = request.form['status']
-        story = {'id': id, 'title': title, 'user_story': user_story, 'acceptance_criteria': acceptance_criteria,
-                 'business_value': business_value, 'estimation': estimation, 'status': status}
+    else:
+        id = common.generate_timestamp_as_id()
+        story = common.make_story(id)
         data_handler.append_stories_in_file(story)
         return redirect('/')
 
 
-@app.route('/')
-@app.route('/list')
-@app.route('/story/<story_id>')
-def route_edit(story_id, methods=['GET', 'POST']):
-    if request.method == 'GET':
-        wanted_story = data_handler.get_story_by_id(story_id)
+@app.route('/story/<id>', methods=['GET', 'POST'])
+def route_edit(id):
+    """
+    Function support webpage /story/<id> - editing and updating
+    finds story by id (timestamp)
+    Using two methods - if 'GET' - shows form with filled data from story
+    if POST - execute saving updated data in database and redirect to /list page
+    :param id:
+    :return: depends of used method (but webpage)
+    """
+    if request.method == "GET":
+        wanted_story = data_handler.get_story_by_id(id)
         return render_template('edit.html', wanted_story=wanted_story)
     else:
-        return 'Kocham cie'
-
+        new_story = common.make_story(id)
+        data_handler.update_story_by_id(id, new_story)
+        return redirect('/')
 
 
 if __name__ == '__main__':
