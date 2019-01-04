bb835afb63fcf8f8dc8a7fa6b3f2aea4c178662c
magg666 <mw23127@gmail.com>
2019-01-04 03:16:37 +0100

    added new function for edit - still needs work

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def route_list():
     return render_template('list.html', user_stories=user_stories, headers=headers)
 
 
-@app.route('/add-story', methods=['GET', 'POST'])
+@app.route('/story', methods=['GET', 'POST'])
 def route_add():
     statuses = data_handler.STATUSES
     story = None
@@ -24,13 +24,25 @@ def route_add():
         title = request.form['title']
         user_story = request.form['user_story']
         acceptance_criteria = request.form['acceptance_criteria']
-        business_value = request.form['business_value']
-        estimation = request.form['estimation']
+        business_value = request.form['business_value'] + ' point'
+        estimation = request.form['estimation'] + ' h'
         status = request.form['status']
         story = {'id': id, 'title': title, 'user_story': user_story, 'acceptance_criteria': acceptance_criteria,
                  'business_value': business_value, 'estimation': estimation, 'status': status}
         data_handler.append_stories_in_file(story)
         return redirect('/')
+
+
+@app.route('/')
+@app.route('/list')
+@app.route('/story/<story_id>')
+def route_edit(story_id, methods=['GET', 'POST']):
+    if request.method == 'GET':
+        wanted_story = data_handler.get_story_by_id(story_id)
+        return render_template('edit.html', wanted_story=wanted_story)
+    else:
+        return 'Kocham cie'
+
 
 
 if __name__ == '__main__':
